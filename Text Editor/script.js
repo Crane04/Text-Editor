7c26393f8edfbf4e072a31b9f92194083ca96b78
Yusuf Saubana Mayowa <101975553+Crane04@users.noreply.github.com>
2022-09-07 11:53:58 +0100

    Add files via upload

--- Text Editor/script.js
+++ Text Editor/script.js
@@ -10,82 +10,27 @@ const sentence=document.querySelectorAll(".textstyle button")[6]
 const color=document.querySelector("#color")
 const align=document.querySelectorAll(".textstyle button")[8]
 const not=document.querySelector(":not(.nav3)")
+
+//let's save to local storage, you should use an array for this later
+textarea.value=localStorage.getItem("textareavalue")
+
+textarea.style.fontFamily=localStorage.getItem("fontFamily")
+textarea.style.fontSize=localStorage.getItem("fontSize")
+textarea.style.fontWeight= localStorage.getItem("fontWeight")
+bold.style.fontWeight= localStorage.getItem("fontWeight")
+textarea.style.textDecoration=localStorage.getItem("textDecoration") 
+underline.style.textDecoration=localStorage.getItem("textDecoration")        
+textarea.style.fontStyle=localStorage.getItem("italic")
+italic.style.fontStyle=localStorage.getItem("italic")
+color.value=localStorage.getItem("color")
+textarea.style.color=localStorage.getItem("color")
+textarea.style.textTransform=localStorage.getItem("textTransform")
+sentence.style.textTransform=localStorage.getItem("textTransform")
 //define values
 
 let fontsize=14
 let paddin=13
 
-let alignInnerHTML=`
-        
-        <button style="margin:10px" onclick="left()"><i class="fa fa-align-left" aria-hidden="true"></i></button>
-        <button style="margin:10px" onclick="right()"><i class="fa fa-align-right" aria-hidden="true"></i></button>
-        <button style="margin:10px" onclick="center()"><i class="fa fa-align-center" aria-hidden="true"></i></button>
-        <button style="margin:10px" onclick="justify()"><i class="fa fa-align-justify" aria-hidden="true"></i></button>
-`
-function left(){
-    textarea.style.textAlign="left"
-}
-function right(){
-    textarea.style.textAlign="right"
-}
-function center(){
-    textarea.style.textAlign="center"
-}
-function justify(){
-    textarea.style.textAlign="justify"
-}
-let sentenceInnerHTML=`
-        <button style="margin:10px" onclick="nocase()">None</button>
-        <button style="margin:10px" onclick="upperCase()">UPPERCASE</button>
-        <button style="margin:10px" onclick="lowercase()">lowercase</button>
-        <button style="margin:10px" onclick="capitalize()">Capitalize each word</button>
-`
-function nocase(){
-    textarea.style.textTransform="none"
-    sentence.style.textTransform="none"
-}
-function upperCase(){
-    textarea.style.textTransform="uppercase"
-    sentence.style.textTransform="uppercase"
-}
-
-function lowercase(){
-    textarea.style.textTransform="lowercase"
-    sentence.style.textTransform="lowercase"
-}
-
-function capitalize(){
-    textarea.style.textTransform="capitalize"
-    sentence.style.textTransform="capitalize"
-}
-let fontsInnerHTML=`
-        <button>sans-serif</button>
-        <button>Helvatica</button>
-        <button>Sans serif</button>
-        <button>Cambria</button>
-        <button>Impact</button>
-        <button>Arial</button>
-        <button>Curisve</button>
-    `
-    let underlineInnerHTML=`
-        <button onclick="linebeneath()" style="margin:10px"><u>U</u></button>
-        <button onclick="strike()" style="margin:10px"><strike>U</strike></button>
-        <button onclick="overline()" style="text-decoration: overline;margin:10px">U</button>
-    `
-
-    function linebeneath(){
-        textarea.style.textDecoration="underline"
-        underline.style.textDecoration="underline"
-    }
-
-    function strike(){
-        textarea.style.textDecoration="line-through"
-        underline.style.textDecoration="line-through"
-    }
-    function overline(){
-        textarea.style.textDecoration="overline"
-        underline.style.textDecoration="overline"
-    }
 //functions
 
 
@@ -175,11 +120,13 @@ fonts.addEventListener("click", function(){
 increaseText.addEventListener("click", function(){
     fontsize+=5
     textarea.style.fontSize=`${fontsize}px`
+    localStorage.setItem("fontSize",textarea.style.fontSize)
 })
 
 decreaseText.addEventListener("click", function(){
     fontsize-=5
     textarea.style.fontSize=`${fontsize}px`
+    localStorage.setItem("fontSize",textarea.style.fontSize)
 })
 
 bold.addEventListener("click",function(){
@@ -190,6 +137,8 @@ bold.addEventListener("click",function(){
         textarea.style.fontWeight="bold"
         this.style.fontWeight="bold" 
     }
+    
+    localStorage.setItem("fontWeight",textarea.style.fontWeight)
 })
 
     italic.addEventListener("click",function(){
@@ -200,7 +149,137 @@ bold.addEventListener("click",function(){
         textarea.style.fontStyle="italic"
         this.style.fontStyle="italic"
     }
+    localStorage.setItem("italic",textarea.style.fontStyle)
 })
 color.addEventListener("change", function(){
-    textarea.style.color=this.value
+    textarea.style.color=color.value
+    localStorage.setItem("color",color.value)
 })
+
+//local storage
+textarea.addEventListener("input", function(){
+    localStorage.setItem("textareavalue",textarea.value)
+})
+let alignInnerHTML=`
+        
+        <button style="margin:10px" onclick="left()"><i class="fa fa-align-left" aria-hidden="true"></i></button>
+        <button style="margin:10px" onclick="right()"><i class="fa fa-align-right" aria-hidden="true"></i></button>
+        <button style="margin:10px" onclick="center()"><i class="fa fa-align-center" aria-hidden="true"></i></button>
+        <button style="margin:10px" onclick="justify()"><i class="fa fa-align-justify" aria-hidden="true"></i></button>
+`
+textarea.style.textAlign=localStorage.getItem("textAlign")
+align.innerHTML=localStorage.getItem("align")
+function left(){
+    textarea.style.textAlign="left"
+    align.innerHTML=`<i class="fa fa-align-left" aria-hidden="true"></i>`
+    localStorage.setItem("textAlign",textarea.style.textAlign)
+    localStorage.setItem("align", align.innerHTML)
+}
+function right(){
+    textarea.style.textAlign="right"
+    align.innerHTML=`<i class="fa fa-align-right" aria-hidden="true"></i>`
+    localStorage.setItem("textAlign",textarea.style.textAlign)
+    localStorage.setItem("align", align.innerHTML)
+}
+function center(){
+    textarea.style.textAlign="center"
+    align.innerHTML=`<i class="fa fa-align-center" aria-hidden="true"></i>`
+    localStorage.setItem("textAlign",textarea.style.textAlign)
+    localStorage.setItem("align", align.innerHTML)
+}
+function justify(){
+    textarea.style.textAlign="justify"
+    align.innerHTML=`<i class="fa fa-align-justify" aria-hidden="true"></i>` 
+    localStorage.setItem("textAlign",textarea.style.textAlign)
+    localStorage.setItem("align", align.innerHTML)
+}
+let sentenceInnerHTML=`
+        <button style="margin:10px" onclick="nocase()">None</button>
+        <button style="margin:10px" onclick="upperCase()">UPPERCASE</button>
+        <button style="margin:10px" onclick="lowercase()">lowercase</button>
+        <button style="margin:10px" onclick="capitalize()">Capitalize each word</button>
+`
+function nocase(){
+    textarea.style.textTransform="none"
+    sentence.style.textTransform="none"
+    localStorage.setItem("textTransform",textarea.style.textTransform)
+}
+function upperCase(){
+    textarea.style.textTransform="uppercase"
+    sentence.style.textTransform="uppercase"
+    localStorage.setItem("textTransform",textarea.style.textTransform)
+}
+
+function lowercase(){
+    textarea.style.textTransform="lowercase"
+    sentence.style.textTransform="lowercase"
+    localStorage.setItem("textTransform",textarea.style.textTransform)
+}
+
+function capitalize(){
+    textarea.style.textTransform="capitalize"
+    sentence.style.textTransform="capitalize"
+    localStorage.setItem("textTransform",textarea.style.textTransform)
+}
+let fontsInnerHTML=`
+        <button onclick="Lucida()" style="font-family:Lucida-sans;">Lucida-sans</button>
+        <button onclick="sans()" style="font-family:sans-serif;">sans-serif</button>
+        <button onclick="impact()" style="font-family:Impact;">Impact</button>
+        <button onclick="arial()" style="font-family:Arial;">Arial</button>
+        <button onclick="cursive()" style="font-family:cursive;">Curisve</button>
+    `
+    function cursive(){
+        textarea.style.fontFamily="cursive"
+        localStorage.setItem("fontFamily",textarea.style.fontFamily)
+        fonts.style.fontFamily="cursive"
+    }
+
+    function arial(){
+        textarea.style.fontFamily="Arial"
+        localStorage.setItem("fontFamily",textarea.style.fontFamily)
+    }
+    function impact(){
+        textarea.style.fontFamily="Impact"
+        localStorage.setItem("fontFamily",textarea.style.fontFamily)
+    }
+    function helvatica(){
+        textarea.style.fontFamily="Helvatica"
+        localStorage.setItem("fontFamily",textarea.style.fontFamily)
+    }
+    function Lucida(){
+        textarea.style.fontFamily="Lucida Sans Unicode"
+        localStorage.setItem("fontFamily",textarea.style.fontFamily)
+    }
+    function sans(){
+        textarea.style.fontFamily="sans-serif"
+        localStorage.setItem("fontFamily",textarea.style.fontFamily)
+    }
+    let underlineInnerHTML=`
+        <button onclick="noUnderline()" style="margin:10px">U</button>
+        <button onclick="linebeneath()" style="margin:10px"><u>U</u></button>
+        <button onclick="strike()" style="margin:10px"><strike>U</strike></button>
+        <button onclick="overline()" style="text-decoration: overline;margin:10px">U</button>
+    `
+
+    function linebeneath(){
+        textarea.style.textDecoration="underline"
+        underline.style.textDecoration="underline"
+        localStorage.setItem("textDecoration",underline.style.textDecoration) 
+    }
+
+    function strike(){
+        textarea.style.textDecoration="line-through"
+        underline.style.textDecoration="line-through"
+        localStorage.setItem("textDecoration",underline.style.textDecoration)
+    }
+    function overline(){
+        textarea.style.textDecoration="overline"
+        underline.style.textDecoration="overline"
+        localStorage.setItem("textDecoration",underline.style.textDecoration)
+    }
+    function noUnderline(){
+        
+        textarea.style.textDecoration="none"
+        underline.style.textDecoration="none"
+        localStorage.setItem("textDecoration",underline.style.textDecoration)
+    }
